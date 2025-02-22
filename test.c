092fe9ef5af75533ffb37e6cba65724236a7c426
delightlayman <163109448+delightlayman@users.noreply.github.com>
2025-02-22 18:56:28 +0800

    modify-2-22

--- test.c
+++ test.c
@@ -11,5 +11,14 @@ int main(){
     HeapPrint(&hp);
     HeapPush(&hp,13);
     HeapPrint(&hp);
+    int num1=HeapFind(&hp,13);
+    printf("%d\n",num1);
+    int num2=HeapTop(&hp);
+    printf("%d\n",num2);
+    HeapPop(&hp);
+    HeapPrint(&hp);
+    HeapModi(&hp,3,16);
+    HeapPrint(&hp);
+
     return 0;
 }
